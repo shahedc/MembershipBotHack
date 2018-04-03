ac436ea5c3b4e290db7cda73b8219d97782a83cf
shahedc <shahedc@gmail.com>
2018-04-03 11:15:00 -0400

    initial project
    initial project with both Web API and bot enabled

---
+++ MembershipBot/MembershipBot.cs
@@ -0,0 +1,19 @@
+﻿using System.Threading.Tasks;
+using Microsoft.Bot;
+using Microsoft.Bot.Builder;
+using Microsoft.Bot.Schema;
+
+namespace MembershipBot
+{
+    public class MembershipBot : IBot
+    {
+        public async Task OnReceiveActivity(IBotContext context)
+        {
+            if (context.Request.Type is ActivityTypes.Message)
+            {
+                await context.SendActivity($"Hello world.");
+            }
+
+        }
+    }
+}

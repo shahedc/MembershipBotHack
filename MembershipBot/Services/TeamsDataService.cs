4b405ee82d462c0a242c61f89ae53d981eb687c8
James Truitt <jamestruitt@mac.com>
2018-04-03 16:24:07 -0400

    Add TeamsDataService class

---
+++ MembershipBot/Services/TeamsDataService.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MembershipBot.Services
+{
+    public class TeamDataService
+    {
+        public string GetMembersByName(string input)
+        {
+            return string.Empty;
+        }
+
+        public string GetTeamsByName(string input)
+        {
+            return string.Empty;
+        }
+
+        public string GetTeam(int teamID)
+        {
+            return string.Empty;
+        }
+
+        public string GetMember(int memberId)
+        {
+            return string.Empty;
+        }
+
+        public string GetSharedTeams(int memberIda, int memberIdb)
+        {
+            return string.Empty;
+        }
+
+        public string GetRole(int roleId)
+        {
+            return string.Empty;
+        }
+
+        public string GetManagers()
+        {
+            return string.Empty;
+        }
+
+        public string GetManagersForTeams()
+        {
+            return string.Empty;
+        }
+
+        public string GetManagersForMenber(int memberId)
+        {
+            return string.Empty;
+        }
+
+        public string GetTeamsForMenber(int memberId)
+        {
+            return string.Empty;
+        }
+    }
+}
